5b67245179958c91f18111a1f7fac86d7da5de37
Dennis <anariba.hn@hotmail.com>
2024-09-23 16:25:24 -0600

    fix: ignoring files for linter

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,11 +1,5 @@
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-
 export default [
     {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-        ignores: ['node_modules/, build']
-    },
-    { languageOptions: { globals: globals.browser } },
-    ...tseslint.configs.recommended
+        ignores: ['node_modules/*', 'build/*']
+    }
 ];
