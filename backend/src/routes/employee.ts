41812da7fc7ad16f029f7b05ac275e26fb194971
Dennis <anariba.hn@hotmail.com>
2024-10-24 08:45:01 -0600

    fix(backend): resolve patch route path

--- backend/src/routes/employee.ts
+++ backend/src/routes/employee.ts
@@ -9,12 +9,12 @@ const employeeRouter = Router();
 employeeRouter
     .route('/')
     .get((req, res, next) => controller.getAllEmployees(req, res, next))
-    .put((req, res, next) => controller.updateEmployee(req, res, next))
     .post((req, res, next) => controller.createEmployee(req, res, next));
 
 employeeRouter
     .route('/:id')
     .get((req, res, next) => controller.getEmployeeDetail(req, res, next))
+    .put((req, res, next) => controller.updateEmployee(req, res, next))
     .delete((req, res, next) => controller.deleteEmployee(req, res, next));
 
 export default employeeRouter;
